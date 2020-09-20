a759a05a32aedb39decb1f29ae33ea2e167776c2
s277483 <matteo@LAPTOP-2N814JEV.localdomain>
2020-09-20 13:00:37 +0200

    first commit

---
+++ DirectoryWatcher.h
@@ -0,0 +1,35 @@
+//
+// Created by matte on 20/09/2020.
+//
+
+#pragma once
+
+#include <chrono>
+#include <string>
+#include <utility>
+#include <boost/filesystem.hpp>
+#include <thread>
+#include <iostream>
+
+enum class FileStatus {created, modified, erased};
+
+struct RecPath {
+    std::time_t lastEdit;
+    bool isFile;
+};
+
+class DirectoryWatcher {
+    bool running = true;
+    std::string path_to_watch;
+    std::unordered_map<std::string, RecPath> paths_;
+    // Time interval at which we check the base folder for changes
+    std::chrono::duration<int, std::milli> delay;
+
+public:
+
+    // Keep a record of files from the base directory and their last modification time
+    DirectoryWatcher(std::string path_to_watch, std::chrono::duration<int, std::milli> delay);
+
+    // Monitor "path_to_watch" for changes and in case of a change execute the user supplied "action" function
+    void start(const std::function<void (std::string, FileStatus, bool)> &action);
+};
---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "DirectoryWatcher.h"
+
+int main() {
+    // Create a FileWatcher instance that will check the current folder for changes every 5 seconds
+    DirectoryWatcher fw{"../../dir", std::chrono::milliseconds(5000)};
+
+    // Start monitoring a folder for changes and (in case of changes)
+    // run a user provided lambda function
+    fw.start([] (std::string path_to_watch, FileStatus status, bool isFile) -> void {
+        // Process only regular files, all other file types are ignored
+        if(boost::filesystem::is_regular_file(boost::filesystem::path(path_to_watch)) || boost::filesystem::is_directory(boost::filesystem::path(path_to_watch)) || status == FileStatus::erased) {
+            switch(status) {
+                case FileStatus::created:
+                    if (isFile) {
+                        std::cout << "File created: " << path_to_watch << '\n';
+                    } else std::cout << "Directory created: " << path_to_watch << '\n';
+                    break;
+                case FileStatus::modified:
+                    if (isFile) {
+                        std::cout << "File modified: " << path_to_watch << '\n';
+                    } else std::cout << "Directory modified: " << path_to_watch << '\n';
+                    break;
+                case FileStatus::erased:
+                    if (isFile) {
+                        std::cout << "File erased: " << path_to_watch << '\n';
+                    } else std::cout << "Directory erased: " << path_to_watch << '\n';
+                    break;
+                default:
+                    std::cout << "Error! Unknown file status.\n";
+            }
+        } else return;
+
+    });
+}
