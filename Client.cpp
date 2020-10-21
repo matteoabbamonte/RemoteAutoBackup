8c4ca59112e87fb9faaa4000a59ba0d68cdeb77f
MATTEO ABBAMONTE s277483 <s277483@studenti.polito.it>
2020-10-21 11:16:14 +0200

    Client session class integrated into client

--- Client.cpp
+++ Client.cpp
@@ -10,22 +10,122 @@ using boost::asio::ip::tcp;
 class Client {
     boost::asio::io_context& io_context_;
     tcp::socket socket_;
-    std::shared_ptr<Client_Session> cs;
+    Message read_msg_;
+    std::deque<Message> write_queue_c;
+    std::string username;
 
     void do_connect(const tcp::resolver::results_type& endpoints) {
         boost::asio::async_connect(socket_, endpoints,
-                [this](boost::system::error_code ec, const tcp::endpoint&) {
+                [this](boost::system::error_code ec, const tcp::endpoint) {
+                    std::cout << "Entro" << std::endl;
                     if (!ec) {
-                        cs->do_read_size();
+                        do_read_size();
+                    } else {
+                        std::cerr << ec.message() << std::endl;
                     }
                 });
     }
 
+    void create_log_file() {
+        boost::filesystem::ofstream("../../log.txt");
+    }
+
+    void do_read_size() {
+        auto self(std::shared_ptr<Client>(this));
+        int size;
+        boost::asio::async_read(socket_,
+                                boost::asio::buffer(read_msg_.get_size_ptr(), sizeof(size)),
+                                [this, self](boost::system::error_code ec, std::size_t /*length*/)
+                                {
+                                    if (!ec && read_msg_.decode_size())
+                                    {
+                                        do_read_body();
+                                    }
+                                    else
+                                    {
+                                        std::cout << "Size is not a number" << std::endl;
+                                    }
+                                });
+    }
+
+    void do_read_body() {
+        auto self(std::shared_ptr<Client>(this));
+        boost::asio::async_read(socket_,
+                                boost::asio::buffer(read_msg_.get_msg_ptr(), read_msg_.get_size_int()),
+                                [this, self](boost::system::error_code ec, std::size_t /*length*/)
+                                {
+                                    if (!ec)
+                                    {
+                                        read_msg_.decode_message();
+                                        //change header to status
+                                        auto header = static_cast<status_type>(read_msg_.get_header());
+                                        std::string data = read_msg_.get_data();
+                                        //statusQueue.push_status(header, data);
+                                        status_handler(header, data);
+                                        do_read_size();
+                                    }
+                                    else {
+                                        std::cout << ec.message() << std::endl;
+                                    }
+                                });
+    }
+
+    void status_handler(int status, std::string data) {
+        boost::filesystem::ofstream outFile;
+        outFile.open("../../log.txt", std::ios::app);
+        data.append("\n");
+        outFile.write(data.data(), data.size());
+        outFile.close();
+        if (status_type::in_need) {
+            // copiare il comando che faremo nello switch del main
+        }
+    }
+
+    void do_write() {
+        auto self(std::shared_ptr<Client>(this));
+        boost::asio::async_write(socket_,
+                                 boost::asio::buffer(write_queue_c.front().get_msg_ptr(),
+                                                     write_queue_c.front().get_size_int()),
+                                 [this, self](boost::system::error_code ec, std::size_t /*length*/)
+                                 {
+                                     if (!ec)
+                                     {
+                                         write_queue_c.pop_front();
+                                         if (!write_queue_c.empty())
+                                         {
+                                             do_write();
+                                         }
+                                     }
+                                     else
+                                     {
+                                         std::cout << ec.message() << std::endl;
+                                     }
+                                 });
+    }
+
+    void enqueue_msg(const Message &msg) {
+        write_queue_c.emplace_back(msg);
+        do_write();
+    }
+
+    void get_credentials() {
+        std::cout << "Insert username: ";
+        std::getline(std::cin, this->username);
+        std::string password;
+        std::cout << "Insert password: ";
+        std::getline(std::cin, password);
+        Message write_msg;
+        write_msg.put_credentials(this->username, password);
+        write_msg.encode_header(0);
+        write_msg.zip_message();
+        enqueue_msg(write_msg);
+    }
+
 public:
-    Client(boost::asio::io_context& io_context, const tcp::resolver::results_type& endpoints) : io_context_(io_context), socket_(io_context), cs(new Client_Session(io_context)) {
+    Client(boost::asio::io_context& io_context, const tcp::resolver::results_type& endpoints) : io_context_(io_context), socket_(io_context) {
         do_connect(endpoints);
         //std::cout <<  << std::endl;
-        cs->get_credentials();
+        get_credentials();
     }
 
     static void send_actions(std::string path_to_watch, FileStatus status, bool isFile) {
@@ -73,9 +173,13 @@ int main(int argc, char* argv[]) {
 
     Client cl(io_context, endpoints);
 
+    std::thread t([&io_context](){ io_context.run(); });
+
     // Start monitoring a folder for changes and (in case of changes)
     // run a user provided lambda function
     fw.start(Client::send_actions);
+
+    t.join();
 }
 
 
